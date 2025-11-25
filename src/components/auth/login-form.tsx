bacfd92214d24d31ac162192f25ccdd465a15dd2
louis0113 <luizhenrique011304@gmail.com>
2025-11-25 13:52:51 -0300

    fix: final fix on loginForm component

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -1,14 +1,14 @@
 "use client";
-
 import * as z from "zod";
 import { CardWrapper } from "@/components/auth/card-wrapper";
 import { Input } from "@/components/ui/input";
-import { useForm } from "react-hook-form";
+import { ControllerRenderProps, useForm } from "react-hook-form";
 import { LoginSchema } from "@/schemas";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Field,
   FieldDescription,
+  FieldError,
   FieldGroup,
   FieldLabel,
   FieldLegend,
@@ -18,20 +18,19 @@ import {
 import { LoginButton } from "@/components/auth/login-button";
 import { Button } from "@/components/ui/button";
 import { Controller } from "react-hook-form";
+
 export const LoginForm = () => {
-
-type FormValues = z.infer<typeof LoginSchema>
-
-  const  form  = useForm< FormValues>({
+  type FormValues = z.infer<typeof LoginSchema>;
+  const form = useForm<FormValues>({
     resolver: zodResolver(LoginSchema),
     defaultValues: {
       email: "",
       password: "",
     },
   });
-  
-  function onSubmit(data : FormValues) {
-    console.log(data)
+
+  function onSubmit(data: FormValues) {
+    console.log(data);
   }
 
   return (
@@ -42,49 +41,71 @@ type FormValues = z.infer<typeof LoginSchema>
       showSocial
     >
       <form onSubmit={form.handleSubmit(onSubmit)}>
-      <FieldSet >
-        <FieldGroup>
-            <Controller name="email" control={form.control} render={({field, fieldState}) => {
-<Field>
-            <FieldLabel htmlFor="email">Email</FieldLabel>
-            <Input {...field} placeholder="some@email.com" />
-            <FieldDescription>Put your email on your account.</FieldDescription>
-          </Field>
-
-            }}/>
-          
-          <Field>
-            <FieldLabel htmlFor="password">Password</FieldLabel>
-            <FieldDescription>
-              Must be at least 8 characters long.
-            </FieldDescription>
-            <Input
-              
-              type="password"
-              placeholder="••••••••"
+        <FieldSet>
+          <FieldGroup>
+            <Controller
+              name="email"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="email">Email</FieldLabel>
+                  <Input
+                    {...field}
+                    placeholder="some@email.com"
+                    aria-invalid={fieldState.invalid}
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                  <FieldDescription>
+                    Put your email on your account.
+                  </FieldDescription>
+                </Field>
+              )}
             />
-        
-          </Field >
+            <Controller
+              name="password"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="password">Password</FieldLabel>
+                  <FieldDescription>
+                    Must be at least 8 characters long.
+                  </FieldDescription>
+                  <Input
+                    {...field}
+                    type="password"
+                    placeholder="••••••••"
+                    aria-invalid={fieldState.invalid}
+                  />
 
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
             <Field orientation="vertical">
               <LoginButton mode="redirect" onClick="">
-
-  <Button className="w-full" variant="outline"  size="lg" type="reset">
-    Reset
-  </Button>
-</LoginButton>
-<LoginButton mode="redirect" onClick="">
-
-  <Button className="w-full" size="lg" type="submit">
-    Log in
-  </Button>
-</LoginButton>
-
+                <Button
+                  className="w-full"
+                  variant="outline"
+                  size="lg"
+                  onClick={() => {
+                    form.reset();
+                  }}
+                >
+                  Reset
+                </Button>
+              </LoginButton>
+              <LoginButton mode="redirect" onClick="">
+                <Button className="w-full" size="lg" type="submit">
+                  Log in
+                </Button>
+              </LoginButton>
             </Field>
-        </FieldGroup>
-      </FieldSet>
-
-
+          </FieldGroup>
+        </FieldSet>
       </form>
     </CardWrapper>
   );
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,6 +1,6 @@
 import * as z from "zod";
 
 export const LoginSchema = z.object({
-  email: z.string().email({ message  :"Email is required"}),
-  password: z.string({message : "Password is required"}),
+  email: z.email({ message: "Invalid email address" }),
+  password: z.string().min(6, "Min length is 6 characters"),
 });
